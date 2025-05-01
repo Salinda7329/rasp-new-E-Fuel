3cbd398c9037d2b966a1804abba5be373ae7fae6
SalindaGIT <salindakas@gmail.com>
2025-05-01 13:21:23 +0530

    test cameras @ pygame-test.py

--- pygame-test.py
+++ pygame-test.py
@@ -1,14 +1,4 @@
-import pygame
 import pygame.camera
 
 pygame.camera.init()
-
-cam = pygame.camera.Camera("/dev/video0", (640,480))
-
-cam.start()
-
-image = cam.get_image()
-
-pygame.image.save(image, "photo.jpg")
-
-cam.stop()
+print("Available cameras:", pygame.camera.list_cameras())
