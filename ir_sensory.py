e2bea54d4ba6e8328ccac7ac10c321ac7f8d97a0
Salinda7329 <salindakas@gmail.com>
2025-05-03 15:30:23 +0530

    Modified ir sensor test file

--- ir_sensory.py
+++ ir_sensory.py
@@ -2,11 +2,14 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+#IR1 = 13
+#IR2 = 16
+#IR3 = 15
 
 try:
     while True:
-        if GPIO.input(13) == GPIO.LOW:
+        if GPIO.input(16) == GPIO.LOW:
             print("Vehicle detected!")
         else:
             print("No vehicle")
